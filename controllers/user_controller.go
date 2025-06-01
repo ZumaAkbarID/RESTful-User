87221aadb08ae3124008ca295b886b940215b757
ZumaAkbarID <rahmatwahyumaakbar@gmail.com>
2025-06-01 12:08:54 +0700

    feat: update user

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -91,3 +91,59 @@ func FindUserById(c *gin.Context) {
 		},
 	})
 }
+
+func UpdateUser(c *gin.Context) {
+
+	id := c.Param("id")
+
+	var user models.User
+
+	if err := database.DB.First(&user, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, structs.ErrorResponse{
+			Success: false,
+			Message: "User not found",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	var req = structs.UserUpdateRequest{}
+
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, structs.ErrorResponse{
+			Success: false,
+			Message: "Invalid request data",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	user.Name = req.Name
+	user.Username = req.Username
+	user.Email = req.Email
+	if req.Password != "" {
+		user.Password = helpers.HashPassword(req.Password)
+	}
+
+	if err := database.DB.Save(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, structs.ErrorResponse{
+			Success: false,
+			Message: "Failed to update user",
+			Errors:  helpers.TranslateErrorMessage(err),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, structs.SuccessResponse{
+		Success: true,
+		Message: "User updated successfully",
+		Data: structs.UserResponse{
+			Id:        user.Id,
+			Name:      user.Name,
+			Username:  user.Username,
+			Email:     user.Email,
+			CreatedAt: user.CreatedAt.Format("2006-01-02 15:04:05"),
+			UpdatedAt: user.UpdatedAt.Format("2006-01-02 15:04:05"),
+		},
+	})
+}
