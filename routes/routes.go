cc3eebf24f739e632104b88bba8c9643aa6efd40
ZumaAkbarID <rahmatwahyumaakbar@gmail.com>
2025-06-01 12:20:58 +0700

    feat: setup cors

--- routes/routes.go
+++ routes/routes.go
@@ -5,11 +5,19 @@ import (
 	"ZumaAkbarID/backend-api/middlewares"
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
+
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:  []string{"*"},
+		AllowMethods:  []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:  []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders: []string{"Content-Length"},
+	}))
 
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
